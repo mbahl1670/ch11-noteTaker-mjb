ec5441085684e0395f32992d28ecff1e1ce9e724
Michael <mbahl1670@gmail.com>
2021-12-18 18:39:39 -0600

    initial get routes written

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const db = require("./db/db.json");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/api/notes", (req, res) => {
+    res.json(db);
+});
+
+app.post("/api/notes", (req, res) => {
+    
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
